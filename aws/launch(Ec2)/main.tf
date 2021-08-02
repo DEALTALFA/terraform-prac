651365670c00177dc324cbee019bf40dfd1c1cb7
Deepak Yadav <alfo7742@gmail.com>
2021-08-02 23:50:30 +0530

    uncomment

--- aws/launch(Ec2)/main.tf
+++ aws/launch(Ec2)/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "firstInstance" {
 	  ami           = "ami-0e306788ff2473ccb"
 	  instance_type = "t2.micro"
   	  key_name="deepak"
- #	  security_groups=["launch-wizard-9"]  
+ 	  security_groups=["launch-wizard-9"]  
 	  tags={
 		Name="firstTF"
 		} 
