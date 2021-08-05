62e8880d74f27aba1db9be655525dc5fb8bcaa5f
Deepak Yadav <alfo7742@gmail.com>
2021-08-06 00:56:20 +0530

    terraform.tfvars file for assigning declared variable directly

---
+++ aws/launch(Ec2)/variable.tf
@@ -0,0 +1,11 @@
+// made seperate file for variable
+
+variable "aws_ami"{
+type=string
+description="variable for AWS image"
+}
+
+variable "aws_instance_type"{
+type=string
+description="variable for instance type"
+}
