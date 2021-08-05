f63bcdcf76d0ac84583717cf61bc0fb6412cafb2
Deepak Yadav <alfo7742@gmail.com>
2021-08-06 01:09:03 +0530

    updated

--- aws/launch(Ec2)/main.tf
+++ aws/launch(Ec2)/main.tf
@@ -1,13 +1,14 @@
 provider "aws" {
-	  region="ap-south-1"
+	region="ap-south-1"
 }
 
 resource "aws_instance" "firstInstance" {
-	  ami           = var.aws_ami
-	  instance_type = var.aws_instance_type
-  	  key_name="deepak"
- 	  security_groups=["launch-wizard-9"]  
-	  tags={
+	ami= var.aws_ami
+	instance_type = var.aws_instance_type
+  	key_name="deepak"
+ 	security_groups=["launch-wizard-9"]  
+	tags={
 		Name="firstTF"
-		} 
+		}
+	  
 }
--- aws/launch(Ec2)/terraform.tfvars
+++ aws/launch(Ec2)/terraform.tfvars
@@ -1,4 +1,5 @@
 // # use .tfvars this file to directly initalise any variable of .tf file
 
-aws_ami="ami-0e306788ff2473ccb"
+/*aws_ami="ami-0e306788ff2473ccb"
 aws_instance_type="t2.small"
+*/
