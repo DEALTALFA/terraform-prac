595e3e4ac162f0566e9cec4f4fe62ad1abf272b5
Deepak Yadav <alfo7742@gmail.com>
2025-08-30 21:50:25 +0530

    update

--- aws/launch(Ec2)/main.tf
+++ aws/launch(Ec2)/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "firstInstance" {
 	ami= var.aws_ami
 	instance_type = var.aws_instance_type
   	key_name="deepak"
- 	security_groups=["launch-wizard-9"]  
+ 	security_groups=["default"]  
 	tags={
 		Name="firstTF"
 	}
