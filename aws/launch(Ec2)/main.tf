6e6fe0073bfb18e21f4769c8d4a1fa0a8a524664
Deepak Yadav <alfo7742@gmail.com>
2026-01-30 23:35:07 +0530

    format

--- aws/launch(Ec2)/main.tf
+++ aws/launch(Ec2)/main.tf
@@ -1,16 +1,16 @@
 provider "aws" {
-	region="ap-south-1"
+  region = "ap-south-1"
 }
 
 
 resource "aws_instance" "firstInstance" {
-	ami= var.aws_ami
-	instance_type = var.aws_instance_type
-  	key_name="deepak"
- 	security_groups=["default"]  
-	tags={
-		Name="firstTF"
-	}
+  ami             = var.aws_ami
+  instance_type   = var.aws_instance_type
+  key_name        = "deepak"
+  security_groups = ["default"]
+  tags = {
+    Name = "firstTF"
+  }
 }
 output "name" {
   value = aws_instance.firstInstance.public_ip
--- aws/launch(Ec2)/variable.tf
+++ aws/launch(Ec2)/variable.tf
@@ -1,10 +1,10 @@
 // made seperate file for variable
 
-variable "aws_ami"{
-type=string
-description="variable for AWS image"
+variable "aws_ami" {
+  type        = string
+  description = "variable for AWS image"
 }
-variable "aws_instance_type"{
-type=string
-description="variable for instance type"
+variable "aws_instance_type" {
+  type        = string
+  description = "variable for instance type"
 }
