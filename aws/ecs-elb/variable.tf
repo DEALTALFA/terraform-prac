04e9e1bad55083618da167e05c7a0a3054e8c0c6
Deepak Yadav <alfo7742@gmail.com>
2026-02-22 23:41:44 +0530

    added task definition and image

--- aws/ecs-elb/variable.tf
+++ aws/ecs-elb/variable.tf
@@ -1,4 +1,13 @@
 variable "ecs_cluster_name" {
-    default = "demo-ecs-cluster"
-    type = string
+  default = "demo-ecs-cluster"
+  type    = string
+}
+variable "container_image" {
+  default = "docker.io/dealtalfa/python-ip:v1"
+  type    = string
+}
+
+variable "task_definition_name" {
+  default = "demo-ecs-python"
+  type = string
 }
